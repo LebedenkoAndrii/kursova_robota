d389c3ad866fead1f4331683b1c1033616b38f51
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-06-09 00:16:19 +0300

    sync moves, added leave func

--- server/server.js
+++ server/server.js
@@ -42,19 +42,44 @@ io.on("connection", (socket) => {
     const room = rooms.find((room) => room.id === roomId);
     if (room) {
       room.gameBoard[cellIndex] = currentPlayer;
+      let result = "";
+      if (checkWin(room.gameBoard, currentPlayer)) {
+        result = `${currentPlayer} wins!`;
+      } else if (checkDraw(room.gameBoard)) {
+        result = "It's a draw!";
+      }
       io.to(roomId).emit("gameResult", {
         gameBoard: room.gameBoard,
         currentPlayer: currentPlayer === "x" ? "o" : "x",
         opponent: "Player",
+        result: result,
       });
-
-      if (checkWin(room.gameBoard)) {
-        io.to(roomId).emit("gameOver", { message: "Game over! You win!" });
-      } else if (checkDraw(room.gameBoard)) {
-        io.to(roomId).emit("gameOver", { message: "Game over! It's a draw!" });
-      }
     }
   });
+
+  const checkWin = (board, currentPlayer) => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let condition of winConditions) {
+      const [a, b, c] = condition;
+      if (
+        board[a] === currentPlayer &&
+        board[a] === board[b] &&
+        board[a] === board[c]
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
 
   socket.on("resetGame", (roomId) => {
     const room = rooms.find((room) => room.id === roomId);
@@ -66,17 +91,25 @@ io.on("connection", (socket) => {
 
   socket.on("deleteRoom", (roomId) => {
     rooms = rooms.filter((room) => room.id !== roomId);
+    io.to(roomId).emit("roomDeleted", {
+      message: "The room has been deleted.",
+    });
     io.emit("roomList", rooms);
   });
 
-  socket.on("gameOver", (roomId) => {
+  socket.on("gameOver", (roomId, winner) => {
     const room = rooms.find((room) => room.id === roomId);
     if (room) {
       room.gameBoard = new Array(9).fill(null);
-      io.to(roomId).emit("gameOver");
+      io.to(roomId).emit("gameOver", winner);
     }
+    console.log("game end");
   });
 });
+
+const checkDraw = (board) => {
+  return board.every((cell) => cell !== null);
+};
 
 server.listen(3001, () => {
   console.log("Server started on port 3001");
